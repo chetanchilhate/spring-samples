f4564a3f41704e7a1e8778aba67ece612ff126a6
chetanchilhate <chilhatechetan04@gmail.com>
2024-08-26 11:30:48 +0530

    add new endpoints to compare reactive with virtual threads
    add new endpoints for multiple calls
    
    add flag to use preview feature
    
    add structured concurrency api
    
    add performance test screenshots
    
    disable banner, enable graceful shutdown

--- virtualthreads/README.md
+++ virtualthreads/README.md
@@ -1,1 +1,57 @@
-![Cost of Platform Thread.png](img.png)
+# Cost of platform thread
+
+![Cost of Platform Thread.png](img/img.png)
+
+# Performance without virtual threads
+
+## Rest Client
+
+![img_1.png](img/img_1.png)
+
+## Reactive
+
+![img_2.png](img/img_2.png)
+
+## Reactive with block
+
+![img_3.png](img/img_3.png)
+
+## Multiple calls with Rest Client
+
+![img_4.png](img/img_4.png)
+
+## Multiple calls with Reactive
+
+![img_5.png](img/img_5.png)
+
+## Multiple calls with Reactive block
+
+![img_6.png](img/img_6.png)
+
+
+# Performance with virtual threads
+
+## Rest Client
+
+![img_7.png](img/img_7.png)
+
+## Reactive
+
+![img_8.png](img/img_8.png)
+
+## Reactive with block
+
+![img_9.png](img/img_9.png)
+
+## Multiple calls with Rest Client
+
+![img_10.png](img/img_10.png)
+
+
+## Multiple calls with Reactive
+
+![img_11.png](img/img_11.png)
+
+## Multiple calls with Reactive block
+
+![img_12.png](img/img_12.png)
--- virtualthreads/src/main/java/com/cj/virtualthreads/Application.java
+++ virtualthreads/src/main/java/com/cj/virtualthreads/Application.java
@@ -1,15 +1,16 @@
 package com.cj.virtualthreads;
 
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.concurrent.StructuredTaskScope;
 
 @SpringBootApplication
 public class Application {
@@ -23,21 +24,79 @@ public class Application {
     return RestClient.create();
   }
 
+  @Bean
+  WebClient createWebClient() {
+    return WebClient.create();
+  }
+
 }
 
-@Slf4j
 @RestController
-@RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 class TodoController {
 
-  RestClient restClient;
+  private static final String SLOW_TODO_URL = "http://localhost:3000/slow-todo";
+  private final WebClient webClient;
+  private final RestClient restClient;
 
-  @GetMapping("api/v1/todo")
+  TodoController(WebClient webClient, RestClient restClient) {
+    this.webClient = webClient;
+    this.restClient = restClient;
+  }
+
+  @GetMapping("api/virtual/todo")
   Todo getTodo() {
-    return restClient.get().uri("http://localhost:3000/todo")
-        .retrieve()
-        .body(Todo.class);
+    return restClient.get().uri(SLOW_TODO_URL).retrieve().body(Todo.class);
+  }
+
+  @GetMapping("api/reactive/todo")
+  Mono<Todo> getTodoReactive() {
+    return webClient.get().uri(SLOW_TODO_URL).retrieve().bodyToMono(Todo.class);
+  }
+
+  @GetMapping("api/block/todo")
+  Todo getTodoBlock() {
+    return webClient.get().uri(SLOW_TODO_URL).retrieve().bodyToMono(Todo.class).block();
+  }
+
+  @GetMapping("api/virtual/todos")
+  List<Todo> getTodos() {
+    var todo1 = restClient.get().uri(SLOW_TODO_URL).retrieve().body(Todo.class);
+
+    var todo2 = restClient.get().uri(SLOW_TODO_URL).retrieve().body(Todo.class);
+
+    return List.of(todo1, todo2);
+  }
+
+  @GetMapping("api/reactive/todos")
+  Mono<List<Todo>> getTodosReactive() {
+    var todo1 = webClient.get().uri(SLOW_TODO_URL).retrieve().bodyToMono(Todo.class);
+
+    var todo2 = webClient.get().uri(SLOW_TODO_URL).retrieve().bodyToMono(Todo.class);
+
+    return Mono.zip(todo1, todo2).map(tuple -> List.of(tuple.getT1(), tuple.getT2()));
+  }
+
+  @GetMapping("api/block/todos")
+  List<Todo> getTodosBlock() {
+    var todo1 = webClient.get().uri(SLOW_TODO_URL).retrieve().bodyToMono(Todo.class);
+
+    var todo2 = webClient.get().uri(SLOW_TODO_URL).retrieve().bodyToMono(Todo.class);
+
+    return Mono.zip(todo1, todo2).map(tuple -> List.of(tuple.getT1(), tuple.getT2())).block();
+  }
+
+  @GetMapping("api/concurrency/todos")
+  List<Todo> getConcurrencyTodos() {
+    try (final var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+      var todo1 = restClient.get().uri(SLOW_TODO_URL).retrieve().body(Todo.class);
+      var todo2 = restClient.get().uri(SLOW_TODO_URL).retrieve().body(Todo.class);
+      var subTaskTodo1 = scope.fork(() -> todo1);
+      var subTaskTodo2 = scope.fork(() -> todo2);
+      scope.join();
+      return List.of(subTaskTodo1.get(), subTaskTodo2.get());
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
