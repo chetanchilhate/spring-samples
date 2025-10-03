fad5bbcb360a583da2b08b258bb480b18ce1ab7b
Chetan Chilhate <chilhatechetan04@gmail.com>
2025-10-03 16:38:28 +0530

    initialize Spring Boot project with Gradle setup and basic chat application structure

---
+++ chat/src/main/java/com/cj/ai/chat/ChatApplication.java
@@ -0,0 +1,31 @@
+package com.cj.ai.chat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@Slf4j
+@SpringBootApplication
+public class ChatApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(ChatApplication.class, args);
+  }
+
+  @Bean
+  ApplicationRunner init(ChatModel chatModel) {
+    return args -> {
+      log.info("Chat Application started...");
+
+      ChatResponse response = chatModel.call(new Prompt("who are you?"));
+
+      log.info("response : {}", response);
+    };
+  }
+
+}
